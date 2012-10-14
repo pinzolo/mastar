bf3778ffe4b0e6f6239e681c86d1994718a06e25
pinzolo <pinzolo@gmail.com>
2012-10-15 04:50:10 +0900

    add feature of adding pairs method refs #37

--- lib/mastar.rb
+++ lib/mastar.rb
@@ -1,7 +1,35 @@
 # coding: utf-8
-require "mastar/version"
-require "mastar/configuration"
+require 'mastar/version'
+require 'mastar/configuration'
+require 'mastar/name_value_pair'
 
 module Mastar
-  # Your code goes here...
+  def self.included(base)
+    base.extend(ClassMethods)
+    base.__send__(:include, InstanceMethods)
+  end
+
+  module ClassMethods
+    def pairs(options = {})
+      opts = options.is_a?(Hash) ? options : {}
+      name = (opts[:name] || opts['name'] || mastar_config.name).to_sym
+      value = (opts[:value] || opts['value'] || mastar_config.value).to_sym
+      self.select([name, value]).map { |r| NameValuePair.new(r.send(name), r.send(value)) }
+    end
+
+    def mastar_name(attr)
+      mastar_config.name = attr.to_sym if attr
+    end
+
+    def mastar_value(attr)
+      mastar_config.value = attr.to_sym if attr
+    end
+
+    def mastar_config
+      @mastar_config ||= Mastar::Configuration.new
+    end
+  end
+
+  module InstanceMethods
+  end
 end
